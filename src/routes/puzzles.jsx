b56382277c35f5c6a0d8adbcb3ee744773e62636
dch00 <david.chang1994@gmail.com>
2023-10-23 17:55:03 -0700

    .

--- src/routes/puzzles.jsx
+++ src/routes/puzzles.jsx
@@ -1,7 +1,7 @@
 export default function Puzzles() {
   return (
-    <div class="puzzles">
-      <div class="Header">Puzzles</div>
+    <div className="puzzles">
+      <div className="bg-primary text-2xl font-bold p-5">Puzzles</div>
       <div class="flex justify-between mt-5">
         <div class="collapse bg-base-200 w-96">
           <div class="collapse-title text-xl font-bold">
@@ -9,28 +9,28 @@ export default function Puzzles() {
             <img
               src="https://i.imgur.com/3tjI9CU.png"
               alt="Placeholder daily puzzle"
-              class="mt-4"
+              className="mt-4"
             />
           </div>
-          <div class="ml-5 mb-5">
+          <div className="ml-5 mb-5">
             <p>Lv. 1 [5 points]</p>
           </div>
         </div>
-        <div class="collapse bg-base-200 w-96">
-          <div class="collapse-title text-xl font-bold">
+        <div className="collapse bg-base-200 w-96">
+          <div className="collapse-title text-xl font-bold">
             <div>Weekly Puzzle</div>
             <img
               src="https://i.imgur.com/8ySDiP7.png"
               alt="Placeholder Weekly Puzzle"
-              class="mt-4"
+              className="mt-4"
             />
           </div>
-          <div class="ml-5 mb-5">
+          <div className="ml-5 mb-5">
             <p>Lv. 2 [10 points]</p>
           </div>
         </div>
       </div>
-      <div class="mt-6 font-bold flex flex-col">
+      <div className="mt-6 font-bold flex flex-col">
         <a target="_blank" href="">
           Puzzle Suggestion Form
         </a>
