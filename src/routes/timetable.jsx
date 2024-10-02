025049c40f90a09efcec24d2c070d3b0462c1907
Min L. <110487459+Min-07-L@users.noreply.github.com>
2024-10-02 02:48:44 +0000

    d

--- src/routes/timetable.jsx
+++ src/routes/timetable.jsx
@@ -1,108 +1,44 @@
 import { useState } from "react";
-const WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const HOURS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const TIMES = new Array(24).fill(0).map((_, i) => i < 12 ? `${HOURS[i % 12]}:00 AM` : `${HOURS[i % 12]}:00 PM`);
 
 export default function Timetable() {
     let [d, setD] = useState(new Date());
     return(
-        <div className="flex flex-col gap-4">
-            <div className="schedule">
-                <table>
-                    <Tablehead weekday = {d.getDay()} />
-                    <Tablebody />
-                </table>
-            </div>
+        <div className="flex flex-col flex-grow min-h-screen" style={{height: "1px"}}>
+            <Schedule />
         </div>
     );
 }
 
-function Tablehead({weekday}) {
-    return(
-        <thead>
-            <tr class="tableRow">
-                <th>{WEEK[weekday]}</th>
-                <th>{WEEK[(weekday+1)%7]}</th>
-                <th>{WEEK[(weekday+2)%7]}</th>
-                <th>{WEEK[(weekday+3)%7]}</th>
-                <th>{WEEK[(weekday+4)%7]}</th>
-                <th>{WEEK[(weekday+5)%7]}</th>
-                <th>{WEEK[(weekday+6)%7]}</th>
-            </tr>
-        </thead>
+function Schedule() {
+    let [calendar, setCalendar] = useState(new Array(7).fill(0).map((_, i) => new Array(24).fill(false)));
+    function toggleCell(day, time) {
+        let temp = [...calendar];
+        temp[day][time] = !(temp[day][time]);
+        setCalendar(temp);
+    }
+    return (
+        <div className="h-full w-full flex justify-center items-center">
+            <div className="flex flex-col pr-1">
+                <div className="h-6"/>
+                {TIMES.map((time) => <div className="flex items-center justify-end text-xs h-6 select-none">{time}</div>)}
+            </div>
+            {calendar.map((times, i) => (<Column key={i} times={times} day={i} toggleCell={toggleCell} />))}
+        </div>
     );
 }
 
-function Tablebody() {
-    return(
-        <tbody>
-            <tr class="tableRow">
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-            </tr>
-            <tr class="tableRow">
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-            </tr>
-            <tr class="tableRow">
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-                <button class="block">
-                    30 min
-                </button>
-            </tr>
-        </tbody>
+function Column({ times, day, toggleCell}) {
+    return (
+        <div>
+            <div className="text-center select-none">{DAYS[day]}</div>
+            {times.map((available, i) => (<Cell key={day + "-" + i} toggleCell={toggleCell} available={available} day={day} index={i} />))}
+        </div>
     );
+}
+
+function Cell({ available, toggleCell, day, index}) {
+    return <div onDragEnter={() => toggleCell(day, index)} className={`h-6 w-12 border ${available ? "bg-success" : ""} $`} />;
 }
